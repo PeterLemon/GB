a683bc0240cc72dc8326caa6562adde4c4e31847
peterlemon <krom@gbadev.org>
2019-03-12 18:58:06 +0000

    APU Lib & Song Demos Update

--- APU/Twinkle/Twinkle.asm
+++ APU/Twinkle/Twinkle.asm
@@ -36,20 +36,29 @@ LoopSong:
   ld bc,SONGCHAN1  // BC = SONGCHAN1 16-Bit Address
 
   APUCHAN1: // APU Channel 1
-    ld a,(bc) // A = Channel 1: Period Table Offset
-    cp REST // Compare A To REST Character ($FF)
+    ld a,(bc)        // A = Channel 1: Period Table Offset
+    cp SUST          // Compare A To SUST Character ($FE)
+    jr z,APUCHAN1End // IF (A == SUST) Channel 1: APUCHAN1 End
+
+    // Channel 1: Key OFF
+    ld hl,NR52_REG   // HL = NR52: Sound On/Off (Channel #1..4 On/Off 0..3, All Channels On/Off 7) ($FF26)
+    res 0,(hl)       // APU Channel 1 Disable
+    cp REST          // Compare A To REST Character ($FF)
     jr z,APUCHAN1End // IF (A == REST) Channel 1: APUCHAN1 End
 
     // ELSE Channel 1: Key ON
-    ld e,a // E = A
+    ld e,a            // E = A
     ld hl,PeriodTable // HL = PeriodTable 16-Bit Address
-    add hl,de // HL += DE
-    ld a,(hl+) // A = Channel 1: Frequency Lo, Increment Period Table Offset
-    ldh (NR13_REG),a // Store Channel 1: Frequency Lo ($FF13) = A
+    add hl,de         // HL += DE
+    ld a,(hl+)        // A = Channel 1: Frequency Lo, Increment Period Table Offset
+    ldh (NR13_REG),a  // Store Channel 1: Frequency Lo ($FF13) = A
 
-    ld a,(hl+) // A = Channel 1: Frequency Hi (Bits 0..3)
-    set 7,a // Set Restart Flag (Bit 7), Reset Length Flag (Bit 6)
-    ldh (NR14_REG),a // Store Channel 1: Restart / Length Flag, Frequency Hi ($FF14) = A
+    ld a,(hl)         // A = Channel 1: Frequency Hi (Bits 0..3)
+    set 7,a           // Set Restart Flag (Bit 7), Reset Length Flag (Bit 6)
+    ldh (NR14_REG),a  // Store Channel 1: Restart / Length Flag, Frequency Hi ($FF14) = A
+
+    ld hl,NR52_REG // HL = NR52: Sound On/Off (Channel #1..4 On/Off 0..3, All Channels On/Off 7) ($FF26)
+    set 0,(hl)     // APU Channel 1 Enable
   APUCHAN1End:
 
   // 250 MS Delay (15 VSYNCS)
@@ -63,12 +72,12 @@ LoopSong:
 
   ld a,SongEnd>>8 // IF (Song Offset != Song End) GOTO APU Channel 1
   cp b
-  jr nz,APUCHAN1
+  jp nz,APUCHAN1
   ld a,SongEnd
   cp c
-  jr nz,APUCHAN1
+  jp nz,APUCHAN1
 
-  jr LoopSong // GOTO Loop Song
+  jp LoopSong // GOTO Loop Song
 
 VBlankInterrupt:
   reti // Return From Interrupt
@@ -78,10 +87,10 @@ PeriodTable: // Period Table Used For APU Note Freqencies
 
 SongStart:
   SONGCHAN1: // APU Channel 1 Song Data At 250ms (15 VSYNCS)
-    db C5, REST, C5, REST, G5, REST, G5, REST, A5, REST, A5, REST, G5, REST, REST, REST // 1. Twinkle Twinkle Little Star...
-    db F5, REST, F5, REST, E5, REST, E5, REST, D5, REST, D5, REST, C5, REST, REST, REST // 2.   How I Wonder What You Are...
-    db G5, REST, G5, REST, F5, REST, F5, REST, E5, REST, E5, REST, D5, REST, REST, REST // 3.  Up Above The World So High...
-    db G5, REST, G5, REST, F5, REST, F5, REST, E5, REST, E5, REST, D5, REST, REST, REST // 4.   Like A Diamond In The Sky...
-    db C5, REST, C5, REST, G5, REST, G5, REST, A5, REST, A5, REST, G5, REST, REST, REST // 5. Twinkle Twinkle Little Star...
-    db F5, REST, F5, REST, E5, REST, E5, REST, D5, REST, D5, REST, C5, REST, REST, REST // 6.   How I Wonder What You Are...
+    db C5, REST, C5, REST, G5, REST, G5, REST, A5, REST, A5, REST, G5, SUST, SUST, REST // 1. Twinkle Twinkle Little Star...
+    db F5, REST, F5, REST, E5, REST, E5, REST, D5, REST, D5, REST, C5, SUST, SUST, REST // 2.   How I Wonder What You Are...
+    db G5, REST, G5, REST, F5, REST, F5, REST, E5, REST, E5, REST, D5, SUST, SUST, REST // 3.  Up Above The World So High...
+    db G5, REST, G5, REST, F5, REST, F5, REST, E5, REST, E5, REST, D5, SUST, SUST, REST // 4.   Like A Diamond In The Sky...
+    db C5, REST, C5, REST, G5, REST, G5, REST, A5, REST, A5, REST, G5, SUST, SUST, REST // 5. Twinkle Twinkle Little Star...
+    db F5, REST, F5, REST, E5, REST, E5, REST, D5, REST, D5, REST, C5, SUST, SUST, REST // 6.   How I Wonder What You Are...
 SongEnd:
--- APU/Twinkle4Channel/Twinkle4Channel.asm
+++ APU/Twinkle4Channel/Twinkle4Channel.asm
@@ -76,20 +76,29 @@ LoopSong:
   ld bc,SONGCHAN1  // BC = SONGCHAN1 16-Bit Address
 
   APUCHAN1: // APU Channel 1
-    ld a,(bc) // A = Channel 1: Period Table Offset
-    cp REST // Compare A To REST Character ($FF)
+    ld a,(bc)        // A = Channel 1: Period Table Offset
+    cp SUST          // Compare A To SUST Character ($FE)
+    jr z,APUCHAN1End // IF (A == SUST) Channel 1: APUCHAN1 End
+
+    // Channel 1: Key OFF
+    ld hl,NR52_REG   // HL = NR52: Sound On/Off (Channel #1..4 On/Off 0..3, All Channels On/Off 7) ($FF26)
+    res 0,(hl)       // APU Channel 1 Disable
+    cp REST          // Compare A To REST Character ($FF)
     jr z,APUCHAN1End // IF (A == REST) Channel 1: APUCHAN1 End
 
     // ELSE Channel 1: Key ON
-    ld e,a // E = A
+    ld e,a            // E = A
     ld hl,PeriodTable // HL = PeriodTable 16-Bit Address
-    add hl,de // HL += DE
-    ld a,(hl+) // A = Channel 1: Frequency Lo, Increment Period Table Offset
-    ldh (NR13_REG),a // Store Channel 1: Frequency Lo ($FF13) = A
+    add hl,de         // HL += DE
+    ld a,(hl+)        // A = Channel 1: Frequency Lo, Increment Period Table Offset
+    ldh (NR13_REG),a  // Store Channel 1: Frequency Lo ($FF13) = A
 
-    ld a,(hl+) // A = Channel 1: Frequency Hi (Bits 0..3)
-    set 7,a // Set Restart Flag (Bit 7), Reset Length Flag (Bit 6)
-    ldh (NR14_REG),a // Store Channel 1: Restart / Length Flag, Frequency Hi ($FF14) = A
+    ld a,(hl)         // A = Channel 1: Frequency Hi (Bits 0..3)
+    set 7,a           // Set Restart Flag (Bit 7), Reset Length Flag (Bit 6)
+    ldh (NR14_REG),a  // Store Channel 1: Restart / Length Flag, Frequency Hi ($FF14) = A
+
+    ld hl,NR52_REG // HL = NR52: Sound On/Off (Channel #1..4 On/Off 0..3, All Channels On/Off 7) ($FF26)
+    set 0,(hl)     // APU Channel 1 Enable
   APUCHAN1End:
 
   ld hl,SongEnd-SongStart
@@ -98,20 +107,29 @@ LoopSong:
   ld c,l
 
   APUCHAN2: // APU Channel 2
-    ld a,(bc) // A = Channel 2: Period Table Offset
-    cp REST // Compare A To REST Character ($FF)
+    ld a,(bc)        // A = Channel 2: Period Table Offset
+    cp SUST          // Compare A To SUST Character ($FE)
+    jr z,APUCHAN2End // IF (A == SUST) Channel 2: APUCHAN2 End
+
+    // Channel 2: Key OFF
+    ld hl,NR52_REG   // HL = NR52: Sound On/Off (Channel #1..4 On/Off 0..3, All Channels On/Off 7) ($FF26)
+    res 1,(hl)       // APU Channel 2 Disable
+    cp REST          // Compare A To REST Character ($FF)
     jr z,APUCHAN2End // IF (A == REST) Channel 2: APUCHAN2 End
 
     // ELSE Channel 2: Key ON
-    ld e,a // E = A
+    ld e,a            // E = A
     ld hl,PeriodTable // HL = PeriodTable 16-Bit Address
-    add hl,de // HL += DE
-    ld a,(hl+) // A = Channel 2: Frequency Lo, Increment Period Table Offset
-    ldh (NR23_REG),a // Store Channel 2: Frequency Lo ($FF18) = A
+    add hl,de         // HL += DE
+    ld a,(hl+)        // A = Channel 2: Frequency Lo, Increment Period Table Offset
+    ldh (NR23_REG),a  // Store Channel 2: Frequency Lo ($FF18) = A
 
-    ld a,(hl+) // A = Channel 2: Frequency Hi (Bits 0..3)
-    set 7,a // Set Restart Flag (Bit 7), Reset Length Flag (Bit 6)
-    ldh (NR24_REG),a // Store Channel 2: Restart / Length Flag, Frequency Hi ($FF19) = A
+    ld a,(hl)         // A = Channel 2: Frequency Hi (Bits 0..3)
+    set 7,a           // Set Restart Flag (Bit 7), Reset Length Flag (Bit 6)
+    ldh (NR24_REG),a  // Store Channel 2: Restart / Length Flag, Frequency Hi ($FF19) = A
+
+    ld hl,NR52_REG // HL = NR52: Sound On/Off (Channel #1..4 On/Off 0..3, All Channels On/Off 7) ($FF26)
+    set 1,(hl)     // APU Channel 2 Enable
   APUCHAN2End:
 
   ld hl,SongEnd-SongStart
@@ -120,24 +138,33 @@ LoopSong:
   ld c,l
 
   APUCHAN3: // APU Channel 3
-    ld a,(bc) // A = Channel 3: Period Table Offset
-    cp REST // Compare A To REST Character ($FF)
+    ld a,(bc)        // A = Channel 3: Period Table Offset
+    cp SUST          // Compare A To SUST Character ($FE)
+    jr z,APUCHAN3End // IF (A == SUST) Channel 3: APUCHAN3 End
+
+    // Channel 3: Key OFF
+    ld hl,NR52_REG   // HL = NR52: Sound On/Off (Channel #1..4 On/Off 0..3, All Channels On/Off 7) ($FF26)
+    res 2,(hl)       // APU Channel 3 Disable
+    cp REST          // Compare A To REST Character ($FF)
     jr z,APUCHAN3End // IF (A == REST) Channel 3: APUCHAN3 End
 
     // ELSE Channel 3: Key ON
-    ld e,a // E = A
+    ld e,a            // E = A
     ld hl,PeriodTable // HL = PeriodTable 16-Bit Address
-    add hl,de // HL += DE
-    ld a,(hl+) // A = Channel 3: Frequency Lo, Increment Period Table Offset
-    ldh (NR33_REG),a // Store Channel 3: Frequency Lo ($FF1D) = A
+    add hl,de         // HL += DE
+    ld a,(hl+)        // A = Channel 3: Frequency Lo, Increment Period Table Offset
+    ldh (NR33_REG),a  // Store Channel 3: Frequency Lo ($FF1D) = A
 
-    ld a,(hl+) // A = Channel 3: Frequency Hi (Bits 0..3)
+    ld a,(hl)         // A = Channel 3: Frequency Hi (Bits 0..3)
     set 6,a // Set Length Flag (Bit 6)
     set 7,a // Set Restart Flag (Bit 7)
-    ldh (NR34_REG),a // Store Channel 3: Restart / Length Flag, Frequency Hi ($FF1E) = A
+    ldh (NR34_REG),a  // Store Channel 3: Restart / Length Flag, Frequency Hi ($FF1E) = A
 
     ld a,128 // Sound Length: Length = 128 (Bit 0..7)
     ldh (NR31_REG),a // Store Channel 3: Sound Length ($FF1B) = A
+
+    ld hl,NR52_REG // HL = NR52: Sound On/Off (Channel #1..4 On/Off 0..3, All Channels On/Off 7) ($FF26)
+    set 2,(hl)     // APU Channel 3 Enable
   APUCHAN3End:
 
   ld hl,SongEnd-SongStart
@@ -146,15 +173,24 @@ LoopSong:
   ld c,l
 
   APUCHAN4: // APU Channel 4
-    ld a,(bc) // A = Channel 4: Period Table Offset
-    cp REST // Compare A To REST Character ($FF)
+    ld a,(bc)        // A = Channel 4: Period Table Offset
+    cp SUST          // Compare A To SUST Character ($FE)
+    jr z,APUCHAN4End // IF (A == SUST) Channel 4: APUCHAN4 End
+
+    // Channel 4: Key OFF
+    ld hl,NR52_REG   // HL = NR52: Sound On/Off (Channel #1..4 On/Off 0..3, All Channels On/Off 7) ($FF26)
+    res 3,(hl)       // APU Channel 4 Disable
+    cp REST          // Compare A To REST Character ($FF)
     jr z,APUCHAN4End // IF (A == REST) Channel 4: APUCHAN4 End
 
     // ELSE Channel 4: Key ON
     ldh (NR43_REG),a // Store Channel 4: Shift Clock Frequency, Counter Step/Width, Dividing Ratio ($FF22) = A
 
-    ld a,%10000000 // A = Channel 4: Set Restart Flag (Bit 7), Reset Length Flag (Bit 6)
+    ld a,%10000000   // A = Channel 4: Set Restart Flag (Bit 7), Reset Length Flag (Bit 6)
     ldh (NR44_REG),a // Store Channel 4: Restart / Length Flag ($FF23) = A
+
+    ld hl,NR52_REG // HL = NR52: Sound On/Off (Channel #1..4 On/Off 0..3, All Channels On/Off 7) ($FF26)
+    set 3,(hl)     // APU Channel 4 Enable
   APUCHAN4End:
 
   ld hl,-((SongEnd-SongStart) * 3)
@@ -173,12 +209,12 @@ LoopSong:
 
   ld a,SongEnd>>8 // IF (Song Offset != Song End) GOTO APU Channel 1
   cp b
-  jr nz,APUCHAN1
+  jp nz,APUCHAN1
   ld a,SongEnd
   cp c
-  jr nz,APUCHAN1
+  jp nz,APUCHAN1
 
-  jr LoopSong // GOTO Loop Song
+  jp LoopSong // GOTO Loop Song
 
 VBlankInterrupt:
   reti // Return From Interrupt
@@ -188,29 +224,29 @@ PeriodTable: // Period Table Used For APU Note Freqencies
 
 SongStart:
   SONGCHAN1: // APU Channel 1 Song Data At 250ms (15 VSYNCS)
-    db C5, REST, C5, REST, G5, REST, G5, REST, A5, REST, A5, REST, G5, REST, REST, REST // 1. Twinkle Twinkle Little Star...
-    db F5, REST, F5, REST, E5, REST, E5, REST, D5, REST, D5, REST, C5, REST, REST, REST // 2.   How I Wonder What You Are...
-    db G5, REST, G5, REST, F5, REST, F5, REST, E5, REST, E5, REST, D5, REST, REST, REST // 3.  Up Above The World So High...
-    db G5, REST, G5, REST, F5, REST, F5, REST, E5, REST, E5, REST, D5, REST, REST, REST // 4.   Like A Diamond In The Sky...
-    db C5, REST, C5, REST, G5, REST, G5, REST, A5, REST, A5, REST, G5, REST, REST, REST // 5. Twinkle Twinkle Little Star...
-    db F5, REST, F5, REST, E5, REST, E5, REST, D5, REST, D5, REST, C5, REST, REST, REST // 6.   How I Wonder What You Are...
+    db C5, REST, C5, REST, G5, REST, G5, REST, A5, REST, A5, REST, G5, SUST, SUST, REST // 1. Twinkle Twinkle Little Star...
+    db F5, REST, F5, REST, E5, REST, E5, REST, D5, REST, D5, REST, C5, SUST, SUST, REST // 2.   How I Wonder What You Are...
+    db G5, REST, G5, REST, F5, REST, F5, REST, E5, REST, E5, REST, D5, SUST, SUST, REST // 3.  Up Above The World So High...
+    db G5, REST, G5, REST, F5, REST, F5, REST, E5, REST, E5, REST, D5, SUST, SUST, REST // 4.   Like A Diamond In The Sky...
+    db C5, REST, C5, REST, G5, REST, G5, REST, A5, REST, A5, REST, G5, SUST, SUST, REST // 5. Twinkle Twinkle Little Star...
+    db F5, REST, F5, REST, E5, REST, E5, REST, D5, REST, D5, REST, C5, SUST, SUST, REST // 6.   How I Wonder What You Are...
 SongEnd:
 
   SONGCHAN2: // APU Channel 2 Song Data At 250ms (15 VSYNCS)
-    db C3, REST, C3, REST, G3, REST, G3, REST, A3, REST, A3, REST, G3, REST, REST, REST // 1.
-    db F3, REST, F3, REST, E3, REST, E3, REST, D3, REST, D3, REST, C3, REST, REST, REST // 2.
-    db G3, REST, G3, REST, F3, REST, F3, REST, E3, REST, E3, REST, D3, REST, REST, REST // 3.
-    db G3, REST, G3, REST, F3, REST, F3, REST, E3, REST, E3, REST, D3, REST, REST, REST // 4.
-    db C3, REST, C3, REST, G3, REST, G3, REST, A3, REST, A3, REST, G3, REST, REST, REST // 5.
-    db F3, REST, F3, REST, E3, REST, E3, REST, D3, REST, D3, REST, C3, REST, REST, REST // 6.
+    db C3, REST, C3, REST, G3, REST, G3, REST, A3, REST, A3, REST, G3, SUST, SUST, REST // 1.
+    db F3, REST, F3, REST, E3, REST, E3, REST, D3, REST, D3, REST, C3, SUST, SUST, REST // 2.
+    db G3, REST, G3, REST, F3, REST, F3, REST, E3, REST, E3, REST, D3, SUST, SUST, REST // 3.
+    db G3, REST, G3, REST, F3, REST, F3, REST, E3, REST, E3, REST, D3, SUST, SUST, REST // 4.
+    db C3, REST, C3, REST, G3, REST, G3, REST, A3, REST, A3, REST, G3, SUST, SUST, REST // 5.
+    db F3, REST, F3, REST, E3, REST, E3, REST, D3, REST, D3, REST, C3, SUST, SUST, REST // 6.
 
   SONGCHAN3: // APU Channel 3 Song Data At 250ms (15 VSYNCS)
-    db C7, REST, C7, REST, G7, REST, G7, REST, A7, REST, A7, REST, G7, REST, REST, REST // 1.
-    db F7, REST, F7, REST, E7, REST, E7, REST, D7, REST, D7, REST, C7, REST, REST, REST // 2.
-    db G7, REST, G7, REST, F7, REST, F7, REST, E7, REST, E7, REST, D7, REST, REST, REST // 3.
-    db G7, REST, G7, REST, F7, REST, F7, REST, E7, REST, E7, REST, D7, REST, REST, REST // 4.
-    db C7, REST, C7, REST, G7, REST, G7, REST, A7, REST, A7, REST, G7, REST, REST, REST // 5.
-    db F7, REST, F7, REST, E7, REST, E7, REST, D7, REST, D7, REST, C7, REST, REST, REST // 6.
+    db C7, REST, C7, REST, G7, REST, G7, REST, A7, REST, A7, REST, G7, SUST, SUST, REST // 1.
+    db F7, REST, F7, REST, E7, REST, E7, REST, D7, REST, D7, REST, C7, SUST, SUST, REST // 2.
+    db G7, REST, G7, REST, F7, REST, F7, REST, E7, REST, E7, REST, D7, SUST, SUST, REST // 3.
+    db G7, REST, G7, REST, F7, REST, F7, REST, E7, REST, E7, REST, D7, SUST, SUST, REST // 4.
+    db C7, REST, C7, REST, G7, REST, G7, REST, A7, REST, A7, REST, G7, SUST, SUST, REST // 5.
+    db F7, REST, F7, REST, E7, REST, E7, REST, D7, REST, D7, REST, C7, SUST, SUST, REST // 6.
 
   SONGCHAN4: // APU Channel 4 Song Data At 250ms (15 VSYNCS)
     db NA1, REST, N61, REST, N21, REST, N21, REST, NA1, REST, N61, REST, N21, REST, N21, REST // 1.
